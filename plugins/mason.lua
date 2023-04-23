e9941e78249b4ee5834515b63d05598b851d50bb
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-04-23 13:05:45 +0300

    add emmet to Mason auto install

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "lua_ls", "tsserver", "cssls", "html", "jsonls", "eslint" },
+      ensure_installed = { "lua_ls", "tsserver", "cssls", "html", "jsonls", "eslint", "emmet_ls" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
